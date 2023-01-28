d83c8d5b8a43a2ee884364d9c91acbbdf39447dd
Sam Key <samkey726@gmail.com>
2023-01-28 16:59:45 +0000

    template commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>My First Vue App</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
+
 </script>
 
 <style>
