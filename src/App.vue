a4b04d1448be0709310abc19226881dcd29efe93
Sam Key <samkey726@gmail.com>
2023-01-29 00:48:59 +0000

    changed layout and heading

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <Header></Header>
-  <h1>Hi</h1>
+  <div class="main">
+    <h1>SAM KEY</h1>
+    <Header></Header>
+    <div class="content">
+      <h1>Hi</h1>
+    </div>
+  </div>
 </template>
 
 <script> 
@@ -12,16 +17,34 @@ export default {
     Header
   }
 }
-
 </script>
 
 <style>
+.content {
+  box-sizing: border-box;
+  border-style: solid;
+  border-width: 0 1px 1px;
+  position: relative;
+  border-color: #e5e5e5; /* USE #e5e5e5; */
+  display: flex;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Verdana,Arial;
+  font-size: 11px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.main {
+  margin-bottom: 0;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  text-align: left;
+  width: 1060px;
 }
 
 html, body {
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,16 @@
 <template>
 <div class="header">
-    <h1>SAM KEY</h1>
-    <div class="header-right">
+    <div class="menu">
         <a v-on:click="Home()">Home</a>
-        <a v-on:click="Home()">ETC</a>
-
-
+        <a v-on:click="Home()">Projects</a>
+        <a v-on:click="Home()">Qualifications</a>
+        <a v-on:click="Home()">Experience</a>
+        <a v-on:click="Home()">Contact</a>
     </div>
 </div>
 </template>
 
 <script>
-
 export default{
     name: 'Header',
     methods : {
@@ -23,25 +22,18 @@ export default{
 </script>
 
 <style>
-.header h1 {
-  width: 200px; 
-  padding-right: 80%;
-  font-size: 27px;
-}
-
 .header {   
   color: white; 
   font-weight: bold;
   overflow: hidden;  
   background: #222;
   display: flex;
+  height: 35px;
 }
 
 .header a {
-  padding: 15px;
+  padding: 8px;
   font-size: 15px;
-  line-height: 25px;
-  border-radius: 25px;
 }
 
 .header a:hover {
@@ -50,11 +42,9 @@ export default{
   cursor: pointer;
 }
 
-/* Float the link section to the right */
-.header-right {
+.menu {
   float: right;
   display: flex;
-  gap: 20px;
-  padding: 5px 40px 5px 0px 
+  gap: 3%;
 }
 </style>
