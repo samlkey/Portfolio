1bcba3ebde45b450e2080267468958048083857e
Sam Key <samkey726@gmail.com>
2024-09-04 19:42:10 +0100

    lang start

--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -1,6 +1,25 @@
 <template>
     <div id="languageScroll">
+        <div class="mainlang-wrapper">
+            <div class="lang-left">
+                <h1 class="lang-left-title">Experience,</h1>
+                <p>
+                    blah blah blah Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda provident dicta quaerat? Placeat, dolores blanditiis animi officiis atque dolore eos quasi commodi tempore iste autem labore maxime obcaecati debitis?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident facere odit quasi iusto atque, consequuntur tenetur deserunt numquam accusantium cum excepturi saepe fugiat corrupti maiores voluptatem, minima reprehenderit, eos dolore?
+                    lor.
+                </p>
 
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quae, totam quibusdam provident quam asperiores officiis, quia molestias ex labore, ullam voluptatum. Exercitationem impedit consectetur rerum eligendi, repudiandae nemo est.
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quae, totam quibusdam provident quam asperiores officiis, quia molestias ex labore, ullam voluptatum. Exercitationem impedit consectetur rerum eligendi, repudiandae nemo est.
+                </p>
+            </div>
+            <div class="lang-right">
+            </div>
+        </div>
     </div>
 </template>
 <script>
